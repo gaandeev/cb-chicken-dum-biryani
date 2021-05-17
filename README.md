1a382cc72caf1cbaa74463d563b5deadbe79d0ca
gaandeev <77530799+gaandeev@users.noreply.github.com>
2021-05-17 09:31:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# cb-chicken-dum-biryani
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ sketch.js
@@ -0,0 +1,18 @@
+Chicken Biryani
+
+ingredients 
+ chicken
+ Biryani rice
+ masalas
+ water 
+ salt
+ curd
+
+ recipes
+ step1 = mix the raw chicken with curd and chilli powder
+ step2 = take a bowl were the biryani rice into the were some water 
+ step3 = swith on the stove and keep the biryani rice in the stove
+ step4 = mix the masalas in the biryani and wait some time the biryani is ready
+ step5 = mix chicken is the biryani and were some water and salt 
+ step6 = wait 30 min the chicken dum biryani is ready 
+            enjoy the eating the biryani 
